e259decaac2584986bb9a320ea8e965de73068f1
igorq937 <igorq937@gmail.com>
2022-12-21 18:05:11 -0300

    Parametrização: calculo de base
    Co-authored-by: Felipe Agustini <felipeboccardi@hotmail.com>

--- simuladorIRPF/test/CalculoImposto.java
+++ simuladorIRPF/test/CalculoImposto.java
@@ -2,7 +2,14 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import exception.DescricaoEmBrancoException;
 import exception.NomeEmBrancoException;
@@ -10,38 +17,90 @@ import exception.ValorDeducaoInvalidoException;
 import exception.ValorRendimentoInvalidoException;
 import src.CalculadoraIRPF;
 
+@RunWith(Parameterized.class)
 public class CalculoImposto {
 	
-	@Test
-	public void testeCalculoBase() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException, ValorDeducaoInvalidoException {
-		CalculadoraIRPF calculadora = new CalculadoraIRPF();
-		calculadora.cadastraRendimento("Investimento", 2000f);
-		calculadora.cadastraOutrasDeducoes("Previdencia Privada", 200f);
-		assertEquals(calculadora.getBaseCalculo(), 1800f, 0.01f);
-		assertEquals(calculadora.getValorImposto(), 0.0f, 0f);
+	CalculadoraIRPF calculadora;
+	Object[][] rendimentos;
+	Object[][] dependentes;
+	Object[][] outrasDeducoes;
+	Object[][] previdencias;
+	float[] pensoes;
+	float valorEsperadoBaseCalculo;
+	float valorEsperadoImposto;
+	
+	public CalculoImposto(Object[][] rendimentos, Object[][] dependentes, Object[][] outrasDeducoes,
+			Object[][] previdencias, float[] pensoes, float valorEsperadoBaseCalculo, float valorEsperadoImposto) {
+		super();
+		this.rendimentos = rendimentos;
+		this.dependentes = dependentes;
+		this.outrasDeducoes = outrasDeducoes;
+		this.previdencias = previdencias;
+		this.pensoes = pensoes;
+		this.valorEsperadoBaseCalculo = valorEsperadoBaseCalculo;
+		this.valorEsperadoImposto = valorEsperadoImposto;
+	}
+	
+	@Before
+	public void setup() {
+		calculadora = new CalculadoraIRPF(); 
+	}
+	
+	@Parameters
+	public static Collection<Object[]> getParameters() {	
+		Object[][] parametros = new Object[][] {
+			{
+				new Object[][] {{"Investimento", 2000f}},
+				new Object[][] {},
+				new Object[][] {{"Previdencia Privada", 200f}},
+				new Object[][] {},
+			 	new float[]	{}, 
+			 	1800f,
+			 	0.0f
+			},
+			{
+				new Object[][] {{"Investimento", 2000f}, {"Salario", 3000f}},
+				new Object[][] {{"Jao", "09/10/2000"}},
+				new Object[][] {{"Previdencia Privada", 200f}},
+				new Object[][] {},
+			 	new float[]	{}, 
+			 	4610.41f,
+			 	401.21f
+			},
+			{
+				new Object[][] {{"Investimento", 2000f}, {"Salario", 3000f}},
+				new Object[][] {{"Jao", "09/10/2000"}},
+				new Object[][] {{"Previdencia Privada", 200f}},
+				new Object[][] {{"Previdencia Obrigatpria", 110f}},
+			 	new float[]	{500f}, 
+			 	4000.41f,
+			 	263.96f
+			},
+		};
+		
+		return Arrays.asList(parametros);
 	}
 	
 	@Test
-	public void testeCalculoImposto2() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException, ValorDeducaoInvalidoException, NomeEmBrancoException {
-		CalculadoraIRPF calculadora = new CalculadoraIRPF();
-		calculadora.cadastraRendimento("Investimento", 2000f);
-		calculadora.cadastraRendimento("Salario", 3000f);
-		calculadora.cadastraOutrasDeducoes("Previdencia Privada", 200f);
-		calculadora.cadastraDependente("Jao", "09/10/2000");
-		assertEquals(calculadora.getBaseCalculo(), 4610.41f, 0.01f);
-		assertEquals(calculadora.getValorImposto(), 401.21f, 0.01f);
-	}
-	
-	@Test
-	public void testeCalculoImposto3() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException, ValorDeducaoInvalidoException, NomeEmBrancoException {
-		CalculadoraIRPF calculadora = new CalculadoraIRPF();
-		calculadora.cadastraRendimento("Investimento", 2000f);
-		calculadora.cadastraRendimento("Salario", 3000f);
-		calculadora.cadastraPensaoAlimenticia(500f);
-		calculadora.cadastraOutrasDeducoes("Previdencia Privada", 200f);
-		calculadora.cadastraDependente("Jao", "09/10/2000");
-		assertEquals(calculadora.getBaseCalculo(), 4110.41, 0.001f);
-		assertEquals(calculadora.getValorImposto(), 288.71f, 0.01f);
+	public void testeCalculoImposto() throws DescricaoEmBrancoException, ValorDeducaoInvalidoException, NomeEmBrancoException, ValorRendimentoInvalidoException {
+		for(Object[] rendimento : rendimentos) {
+			calculadora.cadastraRendimento((String)rendimento[0], (float)rendimento[1]);
+		}
+		for(Object[] dependente: dependentes) {
+			calculadora.cadastraDependente((String)dependente[0], (String)dependente[1]);
+		}
+		for(float pensao: pensoes) {
+			calculadora.cadastraPensaoAlimenticia(pensao);
+		}
+		for(Object[] previdencia: previdencias) {
+			calculadora.cadastraContribuicaoPrevidenciaria((String)previdencia[0], (float)previdencia[1]);
+		}
+		for(Object[] outraDeducao: outrasDeducoes) {
+			calculadora.cadastraOutrasDeducoes((String)outraDeducao[0], (float)outraDeducao[1]);
+		}
+		
+		assertEquals(calculadora.getBaseCalculo(), valorEsperadoBaseCalculo, 0.01f);
+		assertEquals(calculadora.getValorImposto(), valorEsperadoImposto, 0.01f);
 	}
 	
 }
