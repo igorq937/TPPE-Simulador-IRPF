37de38f4f4d49d197e9b251cafcb2218aef7c6f9
igorq937 <igorq937@gmail.com>
2022-12-21 18:28:04 -0300

    Parametrização: calculo da aliquota efetiva
    Co-authored-by: Felipe Agustini <felipeboccardi@hotmail.com>

--- simuladorIRPF/test/CalculoAliquotaEfetiva.java
+++ simuladorIRPF/test/CalculoAliquotaEfetiva.java
@@ -2,33 +2,61 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import exception.DescricaoEmBrancoException;
 import exception.ValorRendimentoInvalidoException;
 import src.CalculadoraIRPF;
 
+@RunWith(Parameterized.class)
 public class CalculoAliquotaEfetiva {
+	
+	CalculadoraIRPF calculadora;
+	Object[] rendimentos;
+	float valorEsperado;
+	
+	
+	public CalculoAliquotaEfetiva(Object[] rendimentos, float valorEsperado) {
+		super();
+		this.rendimentos = rendimentos;
+		this.valorEsperado = valorEsperado;
+	}
+
+
+	@Before
+	public void setup() {
+		calculadora = new CalculadoraIRPF();
+	}
+	
+	@Parameters
+	public static List<Object> getParameters() {	
+		Object[] parametros = new Object[][] {
+			{
+				new Object[] {"Salario", 2000f}, 0.0036f			
+			},
+			{
+				new Object[] {"Salario", 1000f}, 0.0f			
+			},
+			{
+				new Object[] {"Salario", 1903.98f}, 0.0f		
+			},
+		};
+		
+		return Arrays.asList(parametros);
+	}
 	
 	@Test
 	public void testeCalculoAliquotaEfetiva() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException {
-		CalculadoraIRPF calculadora = new CalculadoraIRPF();
-		calculadora.cadastraRendimento("Salario", 2000f);
-		assertEquals(calculadora.getAliquotaEfetiva(), 0.0036f, 0.01f);
-	}
-	
-	@Test
-	public void testeCalculoAliquotaEfetiva2() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException {
-		CalculadoraIRPF calculadora = new CalculadoraIRPF();
-		calculadora.cadastraRendimento("Salario", 1000f);
-		assertEquals(calculadora.getAliquotaEfetiva(), 0.0f, 0.0f);
-	}
-	
-	@Test
-	public void testeCalculoAliquotaEfetiva3() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException {
-		CalculadoraIRPF calculadora = new CalculadoraIRPF();
-		calculadora.cadastraRendimento("Salario", 1903.98f);
-		assertEquals(calculadora.getAliquotaEfetiva(), 0.0f, 0.0f);
+		calculadora.cadastraRendimento((String)rendimentos[0], (float)rendimentos[1]);
+		assertEquals(calculadora.getAliquotaEfetiva(), valorEsperado, 0.01f);
 	}
 	
 }
