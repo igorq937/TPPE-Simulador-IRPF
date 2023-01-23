1b6f10f4b3a67cd307eb86a305184d1ba251a92f
lucasgbezerra <lucasbsb100@gmail.com>
2023-01-23 18:54:27 -0300

    Refactoring: Extrair constante Mau-cheiro identificado: Variáveis que eram atribuidas apenas uma vez, mas não eram definidas como constantes. Descrição da refatoração: Os atributos que eram deviam ser constantes da classe CalculaValorImposto receberam o modificador final. Além disso, os nomes de atributos constantes foram modificados. Classes/métodos/atributos afetados: * Atributo faixas e maxValorFaixas receberam o modificador final. * Atributo faixas renomeado para FAIXAS. * Atributo maxValorFaixas renomeado para MAX_VALOR_FAIXAS.
    Co-authored-by: LucasGlopes <lucasglpes@gmail.com>
    Co-authored-by: Felipe Agustini <felipeboccardi@hotmail.com>
    Co-authored-by: Peh099 <pedrohenriquec099@gmail.com>
    Co-authored-by: Ariel Serafim <arielserafim@gmail.com>
    Co-authored-by: igorq937 <igorq937@gmail.com>

--- simuladorIRPF/src/CalculaValorImposto.java
+++ simuladorIRPF/src/CalculaValorImposto.java
@@ -1,10 +1,9 @@
 package src;
 
 public class CalculaValorImposto {
-
 	private float baseCalculo;
-	private float[] faixas = new float[] {0.0f, 0.075f, 0.15f, 0.225f, 0.275f};
-	private float[] maxValorFaixa = new float[] {1903.98f, 922.67f, 924.40f, 913.63f, (float)Double.POSITIVE_INFINITY};
+	private final float[] FAIXAS = new float[] {0.0f, 0.075f, 0.15f, 0.225f, 0.275f};
+	private final float[] MAX_VALOR_FAIXAS = new float[] {1903.98f, 922.67f, 924.40f, 913.63f, (float)Double.POSITIVE_INFINITY};
 	
 	public CalculaValorImposto(float baseCalculo) {
 		this.baseCalculo = baseCalculo;
@@ -13,14 +12,14 @@ public class CalculaValorImposto {
 	public float computa() {
 		float totalImposto = 0;
 		
-		for (int i = 0; i < maxValorFaixa.length; i++) {
-			if (baseCalculo <= maxValorFaixa[i]) {
-				totalImposto += baseCalculo * faixas[i];
+		for (int i = 0; i < MAX_VALOR_FAIXAS.length; i++) {
+			if (baseCalculo <= MAX_VALOR_FAIXAS[i]) {
+				totalImposto += baseCalculo * FAIXAS[i];
 				break;
 			}else{
-				totalImposto += maxValorFaixa[i] * faixas[i];
+				totalImposto += MAX_VALOR_FAIXAS[i] * FAIXAS[i];
 			}
-			baseCalculo -= maxValorFaixa[i];
+			baseCalculo -= MAX_VALOR_FAIXAS[i];
 		}
 	
 		return totalImposto;
